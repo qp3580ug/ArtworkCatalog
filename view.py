a27007007374530e96b8bdff1d5e3ce319b18403
Nathan W <nathan@email.com>
2019-10-06 11:44:05 -0500

    Added menu and functions in views. created database.py

---
+++ view.py
@@ -0,0 +1,47 @@
+from peewee import *
+from database import Artwork, Artist
+    
+def main():
+    catalogContinue = 'Y'
+    print('Welcome to the National Artist Catalog!')
+    print('Here are the things you can do with this catalog.')
+    print('1) Add a new artist\n2) Search all artwork of a particular artist')
+    print('3) Search all available artwork for an artist\n4) Add a new piece of artwork')
+    print('5) Delete a piece of artwork\n6) Change availability off a piece of artwork')
+    choice = input('What would you like to do? ')
+
+    while catalogContinue.upper() == 'Y':
+        if choice == 1:
+            artistName = input('Please enter an artist name: ')
+            artistEmail = input('Please enter the artist email: ')
+            newArtist = Artist(name = artistName, email = artistEmail)
+            newArtist.save()
+        elif choice == 2:
+            artistSearch = input('Which Artist\'s work would you like to search? ')
+            findArtwork = Artwork.select().where(Artwork.artist == artistSearch)
+            for art in findArtwork:
+                print(art)
+        elif choice == 3:
+            artistSearch = input('Which Artist\'s Available artwork would you like to search? ')
+            findAvailable = Artwork.select().where(Artwork.artist == artistSearch and Artwork.availability == 'Available')
+            for art in findAvailable:
+                print(art)
+        elif choice == 4:
+            newArtArtist = input('Please enter this artworks artist: ')
+            artworkName = input('Please enter the artworks name: ')
+            newPrice = int(input('Please enter the artworks price: '))
+            newAvailability = input('IS this artwork Available or Sold? ')
+            newArtwork = Artwork(artist = newArtArtist, artwork = artworkName, price = newPrice, availability = newAvailability)
+            newArtwork.save()
+        elif choice == 5:
+            artworkSearch = input('What artwork would you like to delete? ')
+            artworkDeleted = Artwork.delete().where(Artwork.artwork == artworkSearch)
+            print('Artwork Deleted: ', artworkDeleted)
+        elif choice == 6:
+            artworkSearch = input('What artwork would you like to change the availability on? ')
+            newAvailability = input('Is this artwork Available or Sold? ')
+            availabilityChanged = Artwork.update(availability = newAvailability).where(Artwork.artwork == artworkSearch).execute()
+            print('The following availability has been changed', availabilityChanged)
+
+if __name__ == '__main__':
+    main()
